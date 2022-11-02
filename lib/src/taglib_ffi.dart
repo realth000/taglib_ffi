0f97853b3693bb8752220be202de83d4c7d59da3
realth000 <a598919335@gmail.com>
2022-11-03 00:39:22 +0800

    fix(ffi): Fix memory leak
    Fix memory leak:
    * Free used pointers.
    * Not fully useful on Windows because character set:
      - Only works with ASCII characters otherwise a 'not found' will
        happen when finding file in TagLib.

--- lib/src/taglib_ffi.dart
+++ lib/src/taglib_ffi.dart
@@ -1,6 +1,8 @@
 import 'dart:ffi';
+import 'dart:io';
 
 import 'package:ffi/ffi.dart';
+import 'package:flutter/services.dart';
 
 import 'taglib_bindings.dart';
 
@@ -38,49 +40,97 @@ class TagLib {
     if (filePath.isEmpty) {
       return null;
     }
-    final tagLib = NativeLibrary(DynamicLibrary.open('libtag_c.so'));
-    final tagFileName = filePath.toNativeUtf8();
-    final tagFile = tagLib.taglib_file_new(tagFileName.cast<Char>());
-    final tagFileTag = tagLib.taglib_file_tag(tagFile);
+    try {
+      final tagLib = NativeLibrary(Platform.isWindows
+          ? DynamicLibrary.open('tag_c.dll')
+          : DynamicLibrary.open('libtag_c.so'));
+      late final Pointer<Char> tagFileName;
+      if (Platform.isWindows) {
+        tagFileName = filePath.toNativeUtf8().cast();
+//        final buffer = StringBuffer();
+//        buffer.write tagFileName =
+//            Pointer.fromAddress(filePath.toNativeUtf8().address);
+        // tagFileName = malloc.allocate<Char>(100);
+        // final listss = latin1.encode(filePath);
+        // int i = 0;
+        // for (; i < listss.length; i++) {
+        //   tagFileName.elementAt(i).value = listss[i];
+        // }
+        // tagFileName.elementAt(i + 1).value = 0;
+      } else {
+        tagFileName = filePath.toNativeUtf8().cast();
+      }
+      final tagFile = tagLib.taglib_file_new(tagFileName);
+      if (tagLib.taglib_file_is_valid(tagFile) == 0) {
+        return null;
+      }
+      final tagFileTag = tagLib.taglib_file_tag(tagFile);
 
-    // Read common properties.
-    // Read Title.
-    final tagFileTagTitle = tagLib.taglib_tag_title(tagFileTag);
-    // Read Artist.
-    final tagFileTagArtist = tagLib.taglib_tag_artist(tagFileTag);
-    // Read AlbumTitle.
-    final tagFileTagAlbum = tagLib.taglib_tag_album(tagFileTag);
-    // Read Track.
-    final tagFileTagTrack = tagLib.taglib_tag_track(tagFileTag);
-    // Read Year.
-    final tagFileTagYear = tagLib.taglib_tag_year(tagFileTag);
-    // Read Genre.
-    final tagFileTagGenre = tagLib.taglib_tag_genre(tagFileTag);
-    // Read Comment
-    final tagFileTagComment = tagLib.taglib_tag_comment(tagFileTag);
+      // Read common properties.
+      // Read Title.
+      final tagFileTagTitle = tagLib.taglib_tag_title(tagFileTag);
+      // Read Artist.
+      final tagFileTagArtist = tagLib.taglib_tag_artist(tagFileTag);
+      // Read AlbumTitle.
+      final tagFileTagAlbum = tagLib.taglib_tag_album(tagFileTag);
+      // Read Track.
+      final tagFileTagTrack = tagLib.taglib_tag_track(tagFileTag);
+      // Read Year.
+      final tagFileTagYear = tagLib.taglib_tag_year(tagFileTag);
+      // Read Genre.
+      final tagFileTagGenre = tagLib.taglib_tag_genre(tagFileTag);
+      // Read Comment
+      final tagFileTagComment = tagLib.taglib_tag_comment(tagFileTag);
 
-    // Read audio properties.
-    final tagProp = tagLib.taglib_file_audioproperties(tagFile);
-    // Read SampleRate;
-    final tagPropSampleRate = tagLib.taglib_audioproperties_samplerate(tagProp);
-    // Read Bitrate.
-    final tagPropBitrate = tagLib.taglib_audioproperties_bitrate(tagProp);
-    // Read Channels.
-    final tagPropChannels = tagLib.taglib_audioproperties_channels(tagProp);
-    // Read Length.
-    final tagPropLength = tagLib.taglib_audioproperties_length(tagProp);
-    return Metadata(
-      title: tagFileTagTitle.cast<Utf8>().toDartString(),
-      artist: tagFileTagArtist.cast<Utf8>().toDartString(),
-      album: tagFileTagAlbum.cast<Utf8>().toDartString(),
-      track: tagFileTagTrack,
-      year: tagFileTagYear,
-      genre: tagFileTagGenre.cast<Utf8>().toDartString(),
-      comment: tagFileTagComment.cast<Utf8>().toDartString(),
-      sampleRate: tagPropSampleRate,
-      bitrate: tagPropBitrate,
-      channels: tagPropChannels,
-      length: tagPropLength,
-    );
+      // Read audio properties.
+      final tagProp = tagLib.taglib_file_audioproperties(tagFile);
+      // Read SampleRate;
+      final tagPropSampleRate =
+          tagLib.taglib_audioproperties_samplerate(tagProp);
+      // Read Bitrate.
+      final tagPropBitrate = tagLib.taglib_audioproperties_bitrate(tagProp);
+      // Read Channels.
+      final tagPropChannels = tagLib.taglib_audioproperties_channels(tagProp);
+      // Read Length.
+      final tagPropLength = tagLib.taglib_audioproperties_length(tagProp);
+
+      final metaData = Metadata(
+        title: tagFileTagTitle.cast<Utf8>().toDartString(),
+        artist: tagFileTagArtist.cast<Utf8>().toDartString(),
+        album: tagFileTagAlbum.cast<Utf8>().toDartString(),
+        track: tagFileTagTrack,
+        year: tagFileTagYear,
+        genre: tagFileTagGenre.cast<Utf8>().toDartString(),
+        comment: tagFileTagComment.cast<Utf8>().toDartString(),
+        sampleRate: tagPropSampleRate,
+        bitrate: tagPropBitrate,
+        channels: tagPropChannels,
+        length: tagPropLength,
+      );
+//      malloc.free(tagProp);
+//      malloc.free(tagFileTagComment);
+//      malloc.free(tagFileTagGenre);
+//      malloc.free(tagFileTagAlbum);
+//      malloc.free(tagFileTagArtist);
+//      malloc.free(tagFileTagTitle);
+//      malloc.free(tagFileTag);
+//      malloc.free(tagFile);
+//      malloc.free(tagFileName);
+
+      tagLib.taglib_tag_free_strings();
+//      tagLib.taglib_free(tagProp.cast<Void>());
+//      tagLib.taglib_free(tagFileTagComment.cast<Void>());
+//      tagLib.taglib_free(tagFileTagGenre.cast<Void>());
+//      tagLib.taglib_free(tagFileTagAlbum.cast<Void>());
+//      tagLib.taglib_free(tagFileTagArtist.cast<Void>());
+//      tagLib.taglib_free(tagFileTagTitle.cast<Void>());
+//      tagLib.taglib_free(tagFileTag.cast<Void>());
+      tagLib.taglib_file_free(tagFile);
+      malloc.free(tagFileName);
+      return metaData;
+    } on PlatformException {
+      print('Failed to read $PlatformException');
+    }
+    return null;
   }
 }
