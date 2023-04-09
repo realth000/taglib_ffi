efa219731f41bfdd8b8462a3994c26d324e566b1
realth000 <a598919335@gmail.com>
2023-04-10 02:26:16 +0800

    build(android): Update build scripts
    Update build scripts:
    * Build without specifiying Android NDK version.

--- Meipuru/script/build_android.sh
+++ Meipuru/script/build_android.sh
@@ -1,30 +1,56 @@
 #!/bin/bash
 
+set -e
+
 if [ $# -lt 1 ];then
-	echo "Usage: $0 [path to save *.so]"
+	echo "Usage: $0 [directory to save *.so]"
+	exit 1
+fi
+
+if [ "x${ANDROID_HOME}" == "x" ];then
+	echo "error: env ANDROID_HOME not set"
 	exit 1
 fi
 
 BUILD_ROOT="$(dirname $0 | xargs realpath)/../build"
 SAVE_ROOT="$(realpath $1)"
 ABIS=("arm64-v8a" "armeabi-v7a" "x86_64" "x86")
+NDK_ROOT=""
 
-if [ -d ${BUILD_ROOT} ];then
-	rm -rf ${BUILD_ROOT}
+if [ ! -d "${ANDROID_HOME}/ndk/" ];then
+	echo "error: Android NDK not installed in ANDROID_HOME(${ANDROID_HOME}). Install Android ndk and try this later"
+	exit 1
+fi
+
+
+NDK_ROOT="$(ls -d ${ANDROID_HOME}/ndk/* | sort | tail -n 1)"
+
+if [ "x${NDK_ROOT}" == "x" ];then
+	echo "error: Can not find a installed Android NDK in ANDROID_HOME\(${ANDROID_HOME}\). Install Android ndk and try this later"
+	exit 1
 fi
 
 if [ -d ${SAVE_ROOT} ];then
 	rm -rf ${SAVE_ROOT}
 fi
 
-mkdir ${SAVE_ROOT}
+if [ ! -f "${BUILD_ROOT}" ];then
+	mkdir "${BUILD_ROOT}"
+fi
+
+if [ ! -f "${SAVE_ROOT}" ];then
+	mkdir "${SAVE_ROOT}"
+fi
 
 for ABI in ${ABIS[@]}
 do
+	if [ -d ${BUILD_ROOT} ];then
+		rm -rf ${BUILD_ROOT}
+	fi
 	mkdir ${BUILD_ROOT}
 	pushd ${BUILD_ROOT}
 	cmake ../ \
-	-DCMAKE_TOOLCHAIN_FILE=~/Android/Sdk/ndk/25.1.8937393/build/cmake/android.toolchain.cmake \
+	-DCMAKE_TOOLCHAIN_FILE="${NDK_ROOT}"/build/cmake/android.toolchain.cmake \
 	-DANDROID_ABI=${ABI} \
 	-DANDROID_PLATFORM=android-33 \
 	-DBUILD_SHARED_LIBS=on \
@@ -37,4 +63,8 @@ do
 	popd
 	rm -rf ${BUILD_ROOT}
 done
+
+find ${SAVE_ROOT} -name "*.so" | xargs "${NDK_ROOT}"/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip
+
+echo "Success, libs saved in directory: ${SAVE_ROOT}"
 
